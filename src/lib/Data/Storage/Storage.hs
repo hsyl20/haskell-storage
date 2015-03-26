88f26c2876539f521921c35b5ead9a67d25bd435
Sylvain HENRY <hsyl20@gmail.com>
2015-03-26 19:58:43 +0100

    Allow reference storage

--- src/lib/Data/Storage/Storage.hs
+++ src/lib/Data/Storage/Storage.hs
@@ -1,22 +1,51 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Data.Storage.Storage
    ( ObjectHash
+   , Ref
    , Storage
    , initStorage
    , readObject
+   , writeObject
    )
 where
 
 import Data.Digest.Pure.SHA
 import Data.SafeCopy
 import Data.Serialize.Get
+import Data.Serialize.Put
+import Data.Binary (encode,decode)
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as LBS
 import System.FilePath
+import System.Directory
+import Control.Monad (when)
+import Control.Applicative ((<$>))
+import Data.Int
 
 -- | Object identifier
-newtype ObjectHash = ObjectHash (Digest SHA256State)
+newtype ObjectHash = ObjectHash (Digest SHA256State) deriving (Eq)
+
+makeHash :: BS.ByteString -> ObjectHash
+makeHash = ObjectHash . sha256 . LBS.fromStrict
+
+sizeOfHash :: Int64
+sizeOfHash = LBS.length (encode hash)
+   where
+      ObjectHash hash = makeHash BS.empty
+
+instance SafeCopy ObjectHash where
+   putCopy (ObjectHash hash) = contain $ putLazyByteString (encode hash)
+   getCopy                   = contain $ (ObjectHash . decode <$> getLazyByteString sizeOfHash)
+   version = 1
+   kind = base
 
 -- | Reference to another object
-newtype Ref a = Ref ObjectHash
+newtype Ref a = Ref ObjectHash deriving (Eq)
+
+deriveSafeCopy 1 'base ''Ref
+
+instance Show (Ref a) where
+   show (Ref (ObjectHash hash)) = "ref-" ++ showDigest hash
 
 
 data Storage = Storage
@@ -25,16 +54,38 @@ data Storage = Storage
 
 
 initStorage :: FilePath -> IO Storage
-initStorage path = return (Storage path)
+initStorage path = do
+   createDirectoryIfMissing True path
+   createDirectoryIfMissing True (path </> "objects")
+   return (Storage path)
 
 computePath :: ObjectHash -> FilePath
 computePath (ObjectHash hash) = "objects" </> showDigest hash
+
+
+
 
 readObject :: SafeCopy a => Storage -> Ref a -> IO a
 readObject storage (Ref hash) = do
 
    bs <- BS.readFile (storagePath storage </> computePath hash)
 
+   when (makeHash bs /= hash) $
+      error "readObject: file has been altered (invalid hash)"
+
    case runGet safeGet bs of
       Left err -> error (show err)
       Right v  -> return v
+
+
+writeObject :: SafeCopy a => Storage -> a -> IO (Ref a)
+writeObject storage obj = do
+
+   let 
+      bs   = runPut (safePut obj)
+      hash = makeHash bs
+
+   BS.writeFile (storagePath storage </> computePath hash) bs
+
+   return (Ref hash)
+
--- src/tests/Test.hs
+++ src/tests/Test.hs
@@ -1,16 +1,36 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 import Data.Storage.Storage
-
-
-main :: IO ()
-main = do
-   putStrLn "test"
+import Data.SafeCopy
+import Data.Word
 
 data MyObject = MyObject
    { field0 :: Word64
    , field1 :: Word64
-   }
+   } deriving (Show)
 
-data MyObject2 = MyObject2
+deriveSafeCopy 1 'base ''MyObject
+
+data MyRefObject = MyRefObject
    { field2 :: Int
    , field3 :: Ref MyObject
-   }
+   } deriving (Show)
+
+deriveSafeCopy 1 'base ''MyRefObject
+
+main :: IO ()
+main = do
+   s <- initStorage "mystore"
+
+   let obj = MyObject 14 18
+   ref <- writeObject s obj
+   putStrLn ("Reference: " ++ show ref)
+   obj2 <- readObject s ref
+   putStrLn (show obj2)
+
+   let obj3 = MyRefObject 14 ref
+   putStrLn (show obj3)
+   ref2 <- writeObject s obj3
+   putStrLn ("Reference: " ++ show ref2)
+   obj4 <- readObject s ref2
+   putStrLn (show obj4)
